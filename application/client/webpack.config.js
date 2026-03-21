22ffc4e13fcc4ec508730e432a68ded8779e1fd7
T.Miura <mtaiga0213@icloud.com>
2026-03-21 11:17:47 +0900

    Reapply "phase 3 done"
    This reverts commit f4eea66405fd5f4aaf8c9ab2a3596c477d2facc6.

--- application/client/webpack.config.js
+++ application/client/webpack.config.js
@@ -30,7 +30,6 @@ const config = {
     main: [
       "core-js",
       "regenerator-runtime/runtime",
-      "jquery-binarytransport",
       path.resolve(SRC_PATH, "./index.css"),
       path.resolve(SRC_PATH, "./buildinfo.ts"),
       path.resolve(SRC_PATH, "./index.tsx"),
@@ -68,10 +67,8 @@ const config = {
   },
   plugins: [
     new webpack.ProvidePlugin({
-      $: "jquery",
       AudioContext: ["standardized-audio-context", "AudioContext"],
       Buffer: ["buffer", "Buffer"],
-      "window.jQuery": "jquery",
     }),
     new webpack.EnvironmentPlugin({
       BUILD_DATE: new Date().toISOString(),
